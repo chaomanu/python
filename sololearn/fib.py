b40ab8c84a311f2474ba094842eb5cfdf6f5c1d9
chaomanu <manumitgitarre@gmail.com>
2020-12-26 15:11:26 +0100

    added fib seq challenge

---
+++ sololearn/fib.py
@@ -0,0 +1,20 @@
+num = int(input("Input number: "))
+
+x = 0
+y = 1
+
+def fib(n,x,y):
+    """ prints the first n numbers of the fibonacci sequence """
+    print(x)
+    x, y = y, x+y
+    n -= 1
+
+    if n < 1:
+        print("STOP")
+        return
+
+    else:
+        fib(n,x,y)
+    
+    
+fib(num,x,y)
