c4965f6a5fb98a937951c3a4e7442a6de5d3d16b
chaomanu <manumitgitarre@gmail.com>
2020-12-28 16:38:38 +0100

    added input for second number

--- trying/exponential.py
+++ trying/exponential.py
@@ -1,12 +1,17 @@
 
-numlist = [i for i in range(11)]
+# prints exponential numbers from 1 to m
+
+print("first number is 1")
+m = int(input("second number "))
+
+numlist = [i for i in range(m+1)]
 print(numlist)
 
 x=0
 y=1
 
 for i in numlist:
-    print(x)
+    print(x, end=", ")
     x = y
     y = x+y
 
