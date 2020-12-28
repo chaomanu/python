e9a07ef57150fb4ee13acd0cb7fe7e4ab9fe1a98
chaomanu <manumitgitarre@gmail.com>
2020-12-28 16:44:56 +0100

    added statements

--- trying/numberlist.py
+++ trying/numberlist.py
@@ -1,5 +1,6 @@
 from functools import reduce
 
+print("let's print the sum of all numbers from first to second number")
 n = int(input("first number "))
 m = int(input("second number "))
 
@@ -9,6 +10,7 @@ print(numberlist)
 #prints a list with numbers from n to m
 
 numsumvar = reduce(lambda n,m: n+m, numberlist)
+print("sum is ", end = "")
 print(numsumvar)
 
 # prints the sum of all the numbers in the list
