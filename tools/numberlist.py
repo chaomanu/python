727b77e490ffbedeeea6e776a392879409c2bf7f
chaomanu <manumitgitarre@gmail.com>
2020-12-30 03:52:26 +0100

    added function for printing numbers

--- tools/numberlist.py
+++ tools/numberlist.py
@@ -28,3 +28,23 @@ evennums = [i for i in range(n,m+1) if i % 2 != 0]
 print(evennums)
 
 # prints only odd numbers
+
+### how about a function?
+#takes two numbers as input and outputs either all, even or odd numbers in that range (including both numbers)
+
+first =  int(input("first number: "))
+second = int(input("second number: ")) + 1
+
+nums = range(first, second)
+type = str(input("all, even or odd: "))
+
+def printnumbers(nums, type):
+    if type == "all":
+        return [num for num in nums]
+    if type == "even":
+        return [num for num in nums if not num % 2]
+    if type == "odd":
+        return [num for num in nums if num % 2]
+
+output = printnumbers(nums, type)
+print(output)
