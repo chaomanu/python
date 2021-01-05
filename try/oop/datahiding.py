f3470cc4fdd1a411cdf844bb7d109cef895c9780
chaomanu <manumitgitarre@gmail.com>
2021-01-05 21:18:25 +0100

    updated

--- try/oop/datahiding.py
+++ try/oop/datahiding.py
@@ -1,13 +1,12 @@
-# from sololearn
-# data hiding, class & static methods
+# data hiding challenge from sololearn
+# private methods & attributes
+# weakly private _oneunderscore > marked as private
+# strongly private __doubleunderscore > can't be accessed outside the class except with _Classname__privatemethod
 
-# private method
-# weakly private method _oneunderscore > marked as private
-# strong private method __doubleunderscore > can't be accessed outside the class except with _Classname__privatemethod
-
+# weakly private
 class BankAccount:
     def __init__(self, balance):
-        self._balance = balance
+        self._balance = balance # balance marked as private
 
     def __repr__(self):
          return "Account Balance: {}".format(self._balance)
@@ -19,3 +18,26 @@ class BankAccount:
 acc = BankAccount(0)
 acc.deposit(int(5))
 print(acc)
+print(acc._balance)
+
+
+#strongly private
+class BankAccount:
+    def __init__(self, balance):
+        self.__balance = balance # balance marked as strongly private
+
+    def __repr__(self):
+         return "Account Balance: {}".format(self.__balance)
+    
+    def deposit(self, amount):
+        self.amount = amount
+        self.__balance += self.amount
+
+acc = BankAccount(0)
+acc.deposit(int(10))
+print(acc)
+print(acc._BankAccount__balance)
+try:
+    print(acc.__balance)
+except(AttributeError):
+    print("can't print private method attribute like that")
