8f3e3f4a26cde3673655a9104607a1366b94c15f
chaomanu <manumitgitarre@gmail.com>
2021-01-05 20:53:23 +0100

    updated

--- calc/simplecalc.py
+++ calc/simplecalc.py
@@ -1,38 +1,31 @@
 class Calculation:
     def __init__(self, number):
         self.number = number
-
     def __add__(self, other):
         self.number += other.number
         return self.number
-
     def __sub__(self,other):
         self.number -= other.number
         return self.number
-
     def __mul__(self,other):
         self.number *= other.number
         return self.number
-
     def __truediv__(self,other):
         self.number /= other.number
         return self.number
-
     def __floordiv__(self,other):
         self.number //= other.number
         return self.number
-
     def __mod__(self,other):
         self.number %= other.number
-        return self.number
-    
+        return self.number 
     def __pow__(self,other):
         self.number **= other.number
         return self.number
 
 while True: 
     try:
-        num1 = Calculation(int(input("first number: ")))
+        num1 = Calculation(float(input("first number: ")))
         break
     except(ValueError):
         print("not a number")
@@ -40,9 +33,11 @@ while True:
 
 ops = ["+", "-", "*", "/", "//", "%", "**"]
 
+type = ""
+
 while True:
     try:
-        type = input("Choose operator: + - * / // % ** ")
+        type = input("Choose operator +, -, *, /, //, %, **; ")
         if type in ops:
             break
         else:
@@ -50,40 +45,36 @@ while True:
             continue
     except:
         print("error")
+        break
 
 while True:
     try:
-        num2 = Calculation(int(input("second number: ")))
+        num2 = Calculation(float(input("second number: ")))
         break
     except(ValueError):
         print("not a number")
         continue
 
+print("result: ", end="")
 
 if type == "+":
     add = num1 + num2
     print(add)
-
 elif type == "-":
     sub = num1 - num2
     print(sub)
-
 elif type == "*":
     sub = num1 * num2
     print(sub)
-
 elif type == "/":
     sub = num1 / num2
     print(sub)
-
 elif type == "//":
     sub = num1 // num2
     print(sub)
-
 elif type == "%":
     sub = num1 % num2
     print(sub)
-
 elif type == "**":
     sub = num1 ** num2
     print(sub)
