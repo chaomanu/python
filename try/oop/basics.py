a35351251aed3e9504e51866b68f5ed4bb7e0798
chaomanu <manumitgitarre@gmail.com>
2021-01-07 17:15:50 +0100

    minor changes

--- try/oop/basics.py
+++ try/oop/basics.py
@@ -13,8 +13,8 @@ oop - objects
 class Classname: 
     classattribute = "classattribute"                # assigns attribute to class that can be accessed from an instances of the class or the class itself
                                                     
-    def __init__ (self, attribute1, attribute2):    # __init__ method - creates an instance (object) of the class (using Classname as a function)
-                                                    #  takes (itc) two arguments and assigns them to the attribute(s), self must always be the first parameter  
+    def __init__ (self, attribute1, attribute2):    # __init__ method - creates an instance (instance method) of the class (using Classname as a function)
+                                                    #  takes one or more arguments and assigns them to the attribute(s), self must always be the first parameter  
                                                     # attributes are accessed with self.attribute           
         self.attribute1 = attribute1                # sets initial value of attribute1 
         self.attribute2 = attribute2                # sets initial value of attribute2
@@ -22,8 +22,8 @@ class Classname:
     def anothermethod (self):                       # other methods (besides the __init__ method) can be added (all must have self as first parameter)
         print("anothermethod")                      # can be accessed the same way as attributes with self.anothermethod() > () because it's a function not an attribute
 
-    def usingclassattribute(self):                  # class accessing classattribute
-        return self.classattribute
+    def usingclassattribute(self):                  
+        return self.classattribute                  # class accessing classattribute
 
 var = Classname("argument1", "argument2")           # uses the class (blueprint) to define an object and bind it to a variable
 var2 = Classname("argument3", "argument4")          # the same class (blueprint) can create different objects with different values
@@ -43,7 +43,7 @@ print(var.usingclassattribute())                    # instance of the class acce
 # classes contain methods (which are functions) 
 # the class describes what the object will be, but is separate from the object itself (like a blueprint)
 # __init__ method = initializer = constructor > creates instance(object) of the class
-# instance = object
+# instances = objects
 # instances of a class have attributes, which are pieces of data associated with them
 # in an __init__ method, self.attribute can be used to set the initial value of an instance's attributes
 # within a method definition, self refers to the instance calling the method
